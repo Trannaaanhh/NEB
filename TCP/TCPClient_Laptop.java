75e8d02b6d094f82c0b1a6f4596c95008ed4e293
Tran Duc Anh <121141638+Trannaaanhh@users.noreply.github.com>
2026-01-01 11:48:25 +0700

    TCP - Object Stream
    [Mã câu hỏi (qCode): kxFJkZzl].  Thông tin sản phẩm vì một lý do nào đó đã bị sửa đổi thành không đúng, cụ thể:
    a) Tên sản phẩm bị đổi ngược từ đầu tiên và từ cuối cùng, ví dụ: “lenovo thinkpad T520” bị chuyển thành “T520 thinkpad lenovo”
    b) Số lượng sản phẩm cũng bị đảo ngược giá trị, ví dụ từ 9981 thành 1899
    
    Một chương trình server cho phép kết nối qua giao thức TCP tại cổng 2209 (hỗ trợ thời gian giao tiếp tối đa cho mỗi yêu cầu là 5s). Yêu cầu là xây dựng một chương trình client tương tác với server sử dụng các luồng đối tượng (ObjectInputStream / ObjectOutputStream) để gửi/nhận và sửa các thông tin bị sai của sản phẩm. Chi tiết dưới đây:
    a) Đối tượng trao đổi là thể hiện của lớp Laptop được mô tả như sau
          •	Tên đầy đủ của lớp: TCP.Laptop
          •	Các thuộc tính: id int, code String, name String, quantity int
          •	Hàm khởi tạo đầy đủ các thuộc tính được liệt kê ở trên
          •	Trường dữ liệu: private static final long serialVersionUID = 20150711L; 
    b)	Tương tác với server theo kịch bản
    1)	Gửi đối tượng là chuỗi chứa mã sinh viên và mã câu hỏi với định dạng "studentCode;qCode". Ví dụ: "B15DCCN999;5AD2B818"
    2)	Nhận một đối tượng là thể hiện của lớp Laptop từ server
    3)	Sửa các thông tin sai của sản phẩm về tên và số lượng.  Gửi đối tượng vừa được sửa sai lên server
    4)	Đóng socket và kết thúc chương trình.

--- TCP/TCPClient_Laptop.java
+++ TCP/TCPClient_Laptop.java
@@ -1,45 +1,42 @@
 package TCP;
-import java.io.*; 
+import java.util.*;
 import java.net.*;
+import java.io.*;
 
-// Class Laptop đưa lên đầu, nén gọn dòng code
-class Laptop implements Serializable {
+class Laptop implements Serializable{
     private static final long serialVersionUID = 20150711L;
-    private int id, quantity; private String code, name; // Gộp khai báo
+    public int id, quantity;
+    public String code, name;
     public Laptop(int id, String code, String name, int quantity) {
-        this.id = id; this.code = code; this.name = name; this.quantity = quantity; // Gộp gán
+        this.id = id; this. code = code; this.name = name; this.quantity = quantity;
     }
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    public int getQuantity() { return quantity; }
-    public void setQuantity(int quantity) { this.quantity = quantity; }
-    public String toString() { return id + " " + code + " " + name + " " + quantity; }
+    public String toString() {return id + " " + code + " " + name + " " + quantity; }
 }
 
 public class TCPClient_Laptop {
-    public static void main(String[] args) throws Exception {
-        String sv = "B22DCVT034", qCode = "kxFJkZzl";
-        try (Socket s = new Socket("203.162.10.109", 2209)) {
+    public static void main(String[] args)throws Exception {
+        String sv = "B22DCVT034", qCode = "kxFJkZzl", ip = "203.162.10.109"; int port = 2209;
+        try(Socket s = new Socket()) {
+            s.connect(new InetSocketAddress(ip, port), 5000);
             s.setSoTimeout(5000);
-            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
             ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
-            // a. Gửi request
-            oos.writeObject(sv + ";" + qCode); oos.flush();
-            // b. Nhận object
+            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+            
+            oos.writeObject(sv + ";" + qCode);
+            oos.flush();
+            
             Laptop lp = (Laptop) ois.readObject();
-            System.out.println("Received: " + lp);
-            // c. Sửa thông tin
-            // Sửa số lượng (đảo ngược)
-            lp.setQuantity(Integer.parseInt(new StringBuilder(String.valueOf(lp.getQuantity())).reverse().toString()));
-            // Sửa tên (đảo đầu cuối)
-            String[] arr = lp.getName().trim().split("\\s+");
-            if (arr.length > 1) {
-                String tmp = arr[0]; arr[0] = arr[arr.length-1]; arr[arr.length-1] = tmp;
-                lp.setName(String.join(" ", arr));
+            
+            lp.quantity = Integer.parseInt(new StringBuilder(String.valueOf(lp.quantity)).reverse().toString());
+            String[] arr = lp.name.trim().split("\\s+");
+            if(arr.length > 1) {
+                String tmp = arr[0]; 
+                arr[0] = arr[arr.length - 1];
+                arr[arr.length - 1] = tmp;
+                lp.name = String.join(" ", arr);
             }
-            // Gửi lại và đóng
-            oos.writeObject(lp); oos.flush();
-            System.out.println("Sent: " + lp);
+            oos.writeObject(lp);
+            oos.flush();
         }
     }
 }
